3091e01dfaa8dcd6cc7e34f08b46ee321db0ba78
NikitaTkachenk <tkachenko.nikita.01@gmail.com>
2026-02-24 20:53:03 +0200

    One more commit

--- Program.cs
+++ Program.cs
@@ -4,16 +4,41 @@ using System.Runtime.CompilerServices;
 
 namespace git_lesson
 {
-    class Program
+
+
+    struct Person
     {
         public string Name {get; private set;} = "";
-        public string FirstName {get; private set;} = "";
+        public string Secondname {get; private set;} = "";
         public int Age {get; private set;}
 
+        public Person(string name, string secondname, int age)
+        {
+            Name = name;
+            Secondname = secondname;
+            Age = age;
+        }
+
+        public void SetPersonInfo(string name, string secondname, int age)
+        {
+            Name = name;
+            Secondname = secondname;
+            Age = age;
+        }
+    }
+
+    class Program
+    {
         static void Main()
         {
-            ConsoleWrite("Hello my friend!");
-            PrintPersonInfo("Nikita", "Tkachenko", 20);
+            Person person = new Person("Nikita", "Tkachenko", 20);
+            PrintPersonInfo(person.Name, person.Secondname, person.Age);
+
+            person.SetPersonInfo("Dima", "Tkachenko", 20);
+            PrintPersonInfo(person.Name, person.Secondname, person.Age);
+
+            person.SetPersonInfo("Kirill", "Bilik", 17);
+            PrintPersonInfo(person.Name, person.Secondname, person.Age);
         }
 
         private static void ConsoleWrite(string text)
