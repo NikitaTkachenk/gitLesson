0bf1a7e248b5d29491978476627c38603a72f472
NikitaTkachenk <tkachenko.nikita.01@gmail.com>
2026-02-24 20:05:26 +0200

    new commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace git_lesson
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConsoleWrite("Hello my friend!");
+        }
+
+        private static void ConsoleWrite(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+
+    }
+}
+
+    
+
+    
+
+
