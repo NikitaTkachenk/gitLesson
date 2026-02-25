95cd376f997fac9ba317a97f06e44e2fc3ae339e
NikitaTkachenk <tkachenko.nikita.01@gmail.com>
2026-02-25 16:24:23 +0200

    new file commit

---
+++ NullableTypeLearn.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace git_lesson
+{
+    class NullableTypeLearn
+    {
+        static void Main()
+        {
+            
+        }
+
+        
+    }
+}
--- obj/Debug/net10.0/git_lesson.AssemblyInfo.cs
+++ obj/Debug/net10.0/git_lesson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("git_lesson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3091e01dfaa8dcd6cc7e34f08b46ee321db0ba78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f569a8dc1a96affdc533f9150fedf93aff71cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("git_lesson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("git_lesson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
